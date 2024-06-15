9ea760dfdce898c8a34a98c66d82794f7ae07618
S. Co1 <sco1.git@gmail.com>
2024-06-15 13:01:18 -0400

    Start some basic tests
    Probably won't be able to cover the actual UI portions but most other things should be testable without needing user input

---
+++ tests/test_helpers.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pytest
+
+from matplotlib_window.base import NUMERIC_T, RectParams, limit_drag, transform_rect_params
+
+PLOTTED_DATA = np.array([1, 2, 3, 4, 5])
+LIMIT_DRAG_TEST_CASES = (
+    (-1, 1),
+    (-1.0, 1),
+    (1, 1),
+    (1.0, 1.0),
+    (3, 3),
+    (3.0, 3),
+    (5, 5),
+    (5.0, 5),
+    (7, 5),
+    (7.0, 5),
+)
+
+
+@pytest.mark.parametrize(("query", "truth_out"), LIMIT_DRAG_TEST_CASES)
+def test_limit_drag(query: NUMERIC_T, truth_out: NUMERIC_T) -> None:
+    assert limit_drag(plotted_data=PLOTTED_DATA, query=query) == pytest.approx(truth_out)
+
+
+def test_transform_rect() -> None:
+    _, ax = plt.subplots()
+    ax.set(xlim=(-10, 10), ylim=(-10, 10))
+    truth_params = RectParams(xy=(0, -10), height=20)
+
+    assert transform_rect_params(ax, 0) == truth_params
