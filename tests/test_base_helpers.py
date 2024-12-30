d1ac15a9a4316af63f8ec58ad135f32b5c04200f
S. Co1 <sco1.git@gmail.com>
2024-12-30 13:05:45 -0500

    Add handling for timedelta-based axis snapping
    See: #8

--- tests/test_base_helpers.py
+++ tests/test_base_helpers.py
@@ -24,6 +24,26 @@ def test_limit_drag(query: NUMERIC_T, truth_out: NUMERIC_T) -> None:
     assert limit_drag(plotted_data=PLOTTED_DATA, query=query) == pytest.approx(truth_out)
 
 
+PLOTTED_TIMEDELTA = np.array([np.timedelta64(i) for i in range(1, 6)])
+NP_TIMEDELTA_LIMIT_DRAG_TEST_CASES = (
+    (-1, 1),
+    (-1.0, 1),
+    (1, 1),
+    (1.0, 1.0),
+    (3, 3),
+    (3.0, 3),
+    (5, 5),
+    (5.0, 5),
+    (7, 5),
+    (7.0, 5),
+)
+
+
+@pytest.mark.parametrize(("query", "truth_out"), NP_TIMEDELTA_LIMIT_DRAG_TEST_CASES)
+def test_limit_np_timedelta_drag(query: NUMERIC_T, truth_out: NUMERIC_T) -> None:
+    assert limit_drag(plotted_data=PLOTTED_TIMEDELTA, query=query) == pytest.approx(truth_out)
+
+
 def test_transform_rect() -> None:
     _, ax = plt.subplots()
     ax.set(xlim=(-10, 10), ylim=(-10, 10))
